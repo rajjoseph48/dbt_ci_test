a2bbb9f5c9eac00c20a4254a5c701a1151ccaf49
Joseph <rajjoseph48@gmail.com>
2025-08-25 00:23:49 +0530

    Added locations models

---
+++ models/staging/stg_locations.sql
@@ -0,0 +1,17 @@
+{{ config(materialized='view') }}
+
+select
+    location_id,
+    location_name,
+    city,
+    state,
+    country,
+    postal_code,
+    warehouse_type,
+    case 
+        when is_active = 'true' then 1 
+        else 0 
+    end as is_active,
+    cast(created_date as date) as created_date,
+    current_timestamp as loaded_at
+from {{ ref('raw_locations') }}
